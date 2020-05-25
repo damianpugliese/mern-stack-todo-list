ad344b3b60bb9a7f4a86afdbb4c9811ad8051d24
damianpugliese <damian.pugliese@gmail>
2020-05-24 22:53:02 -0300

    adding jwt auth

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,20 @@ import React, { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 import NavBar from './ui/layout/NavBar/NavBar';
+import { UncontrolledAlert } from 'reactstrap';
+import PrivateRoute from './ui/components/PrivateRoute/PrivateRoute';
 import ToDoList from './ui/components/ToDoList/ToDoList';
 import Signup from './ui/components/Signup/Signup';
+import Signin from './ui/components/Signin/Signin';
 import { useDispatch } from 'react-redux';
 import { loadUser } from '../src/redux/actions/Users/usersActions';
 import {
     BrowserRouter as Router,
     Switch,
-    Route
+    Route,
+    Redirect 
 } from "react-router-dom";
-import Signin from './ui/components/Signin/Signin';
+import { history } from './helpers/history';
 
 const App = () => {
 
@@ -24,12 +28,16 @@ const App = () => {
 
     return (
         <div className="App">
-            <Router>
+            <Router history={history}>
                 <NavBar />
+                {alert.msg && <UncontrolledAlert color={alert.color}>
+                    {alert.msg}
+                </UncontrolledAlert>}
                 <Switch>
+                    <PrivateRoute exact path="/" component={ToDoList}/>
                     <Route path="/signin" component={Signin}/>
                     <Route path="/signup"component={Signup}/>
-                    <Route path="/tasks" component={ToDoList}/>
+                    <Redirect from='*' to='/'/>
                 </Switch>
             </Router>
         </div>
